74fd496a928ed39ce5dd50ec6d4633bf4b15297c
Zaid <164772082+zka-fdsfo@users.noreply.github.com>
2026-07-05 17:03:04 +0530

    push  the changed

---
+++ server/src/app.js
@@ -0,0 +1,10 @@
+import express from "express"
+import morgan from "morgan";
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!")
+})
+
+export default app
---
+++ server/src/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = async (req, res) => {
+    
+}
+
+export const login = async (req, res) => {
+    
+}
+
+export const logout = async (req, res) => {
+    
+}
