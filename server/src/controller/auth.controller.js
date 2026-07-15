cc26f0de4d4bc17158b1016b348c4b1592d4f151
sawankumar543 <devnagricode@gmail.com>
2026-07-15 04:21:21 -0700

    added maxAge to cookie

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -58,19 +58,20 @@ export const register = async (req, res) => {
 
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-    });
-    
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
+      maxAge: 15 * 60 * 1000, // 15min
     });
 
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30day
+    });
+    
     res.status(201).json({
       success: true,
       message: "User register successfully",
       payload,
       token,
     });
-
   } catch (error) {
     console.log(error);
     res.status(500).json({
